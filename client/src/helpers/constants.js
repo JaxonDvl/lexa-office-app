8e86437c8d7dbc9ef609501afb7857d0952a0c87
JaxonDvl <tatualindvl@gmail.com>
2018-06-20 02:03:57 +0300

    added env variable

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,2 +1,5 @@
-// export const serverUrl = "http://localhost:8080";
-export const serverUrl = "http://lexa.alintatu.com";
+let serverUrl = "http://localhost:8080";
+if(process.env.REACT_HOST) {
+    serverUrl = process.env.REACT_HOST
+}
+export default serverUrl ;
